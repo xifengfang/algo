5b2ea9e10d35d0c7863f386b0602a9fba806abfc
xifengfang <xffang@gmail.com>
2021-03-29 19:42:27 -0400

    Update SkyLine.cs

--- SkyLine.cs
+++ SkyLine.cs
@@ -4,96 +4,94 @@
 
 class Solution
 {
-    private MaxHeap heap;
+    public List<int[]> GetSkyline(int[][] buildings)
+    {
+        int n = buildings.Length;
+        // {x, h, id}
+        List<int[]> es = new List<int[]>();
 
-         public List<int[]> GetSkyline(int[][] buildings)
+
+        for (int i = 0; i < n; ++i)
         {
-            int n = buildings.Length;
-            // {x, h, id}
-            List<int[]> es = new List<int[]>();
-
-
-            for (int i = 0; i < n; ++i)
-            {
-                es.Add(new int[] { buildings[i][0], buildings[i][2], i });
-                es.Add(new int[] { buildings[i][1], -buildings[i][2], i });
-            }
-
-            es.Sort((e1,e2) => { return e1[0] == e2[0] ? e2[1] - e1[1] : e1[0] - e2[0]; });
-
-            List<int[]> ans = new List<int[]>();
-
-            heap = new MaxHeap();
-
-            foreach(var e in es)
-            {
-                int x = e[0];
-                int h = e[1];
-                int id = e[2];
-
-                Boolean entering = h > 0;
-                h = Math.Abs(h);
-
-                if (entering)
-                {
-                    if (h > heap.Max())
-                        ans.Add(new int[] { x, h });
-                    heap.Add(h);
-                }
-                else
-                {
-                    heap.Remove(h);
-                    if (h > heap.Max())
-                        ans.Add(new int[] { x, heap.Max() });
-                }
-            }
-
-            return ans;
+            es.Add(new int[] { buildings[i][0], buildings[i][2]});
+            es.Add(new int[] { buildings[i][1], -buildings[i][2]});
         }
 
-        private class MaxHeap
+        es.Sort((e1,e2) => { return e1[0] == e2[0] ? e2[1] - e1[1] : e1[0] - e2[0]; });
+
+        List<int[]> ans = new List<int[]>();
+
+        var heap = new MaxHeap();
+
+        foreach(var e in es)
         {
-            private SortedSet<int> sortedSet;
-            private IDictionary<int, int> dict;
+            int x = e[0];
+            int h = e[1];
+            int id = e[2];
 
-            public int Count { get; set; }
+            Boolean entering = h > 0;
+            h = Math.Abs(h);
 
-            public MaxHeap()
+            if (entering)
             {
-                sortedSet = new SortedSet<int>();
-                dict = new Dictionary<int, int>();
+                if (h > heap.Max())
+                    ans.Add(new int[] { x, h });
+                heap.Add(h);
             }
-
-            public void Add(int item)
+            else
             {
-                if (!dict.ContainsKey(item))
-                {
-                    dict[item] = 0;
-                }
-                sortedSet.Add(item);
-                dict[item]++;
-                Count++;
-            }
-
-            public int Max()
-            {
-                return sortedSet.Max;
-            }
-
-            public void Remove(int item)
-            {
-                if (!dict.ContainsKey(item))
-                {
-                    return;
-                }
-
-                if (--dict[item] == 0)
-                {
-                    dict.Remove(item);
-                    sortedSet.Remove(item);
-                }
-
-                Count--;
+                heap.Remove(h);
+                if (h > heap.Max())
+                    ans.Add(new int[] { x, heap.Max() });
             }
         }
+
+        return ans;
+    }
+
+    private class MaxHeap
+    {
+        private SortedSet<int> sortedSet;
+        private IDictionary<int, int> dict;
+
+        public int Count { get; set; }
+
+        public MaxHeap()
+        {
+            sortedSet = new SortedSet<int>();
+            dict = new Dictionary<int, int>();
+        }
+
+        public void Add(int item)
+        {
+            if (!dict.ContainsKey(item))
+            {
+                dict[item] = 0;
+            }
+            sortedSet.Add(item);
+            dict[item]++;
+            Count++;
+        }
+
+        public int Max()
+        {
+            return sortedSet.Max;
+        }
+
+        public void Remove(int item)
+        {
+            if (!dict.ContainsKey(item))
+            {
+                return;
+            }
+
+            if (--dict[item] == 0)
+            {
+                dict.Remove(item);
+                sortedSet.Remove(item);
+            }
+
+            Count--;
+        }
+    }
 }
