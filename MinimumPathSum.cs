7838572abbd8580a1162dbe0c5c411c6fedfa228
xifengfang <xffang@gmail.com>
2021-04-03 17:11:46 -0400

    Update MinimumPathSum.cs

--- MinimumPathSum.cs
+++ MinimumPathSum.cs
@@ -3,6 +3,9 @@
 class Solution
 {
       #Recursive with memo.
+      #minPath(0,0)=grid(0,0)
+      #minPath(i,j) = grid(i,j) + min(minPath(i-1,j), minPath(i,j-1))
+      #time, space, O(mn)
       public int MinPathSum(int[][] grid)
       {
           if (grid.Length <= 0)
@@ -23,5 +26,27 @@ class Solution
       }
       
       #DP
-      
+      #minPath(0,0)=grid(0,0)
+      #minPath(0,j)=grid(0,j) + minPath(0, j-1)
+      #minPath(i,0)=grid(i,0) + minPath(i-1, 0)
+      #minPath(i,j) = grid(i,j) + min(minPath(i-1,j), minPath(i,j-1))
+      #time: O(mn), space O(1)
+      public int MinPathSumDP(int[][] grid)
+      {
+            if(grid.Length<=0)
+                  return 0;
+            var rowNum = grid.Length;
+            var colNum = grid[0].Length;
+            for(var i=0; i<rowNum; i++)
+            {
+                  for(var j=0; j<colNum; j++)
+                  {
+                        if(i==0 && j==0) continue;
+                        if(i==0) grid[0][j] += grid[0][j-1];
+                        else if(j==0) grid[i][0] += grid[i-1][0];
+                        else grid[i][j] += Math.Min(grid[i-1][j], grid[i][j-1]);
+                  }
+            }
+            return grid[rowNum-1][colNum-1];
+      }
 }
